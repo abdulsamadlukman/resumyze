ff64f56bd7b53b5aeec6d2256721c54da3c1e3ba
Abdulsamad <optimizetime25@gmail.com>
2026-06-06 08:30:05 +0100

    Next Commit

--- app/Components/ResumeCard.tsx
+++ app/Components/ResumeCard.tsx
@@ -1,74 +1,60 @@
 import { Link } from "react-router";
 import ScoreCircle from "../Components/ScoreCircle";
-import DeleteButton from "./DeleteButton";
 import { useEffect, useState } from "react";
 import { usePuterStore } from "~/lib/puter";
 
-type ResumeCardProps = {
+const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback, imagePath },
+}: {
   resume: Resume;
-  onDelete: (id: string, resumePath?: string, imagePath?: string) => void;
-  deleting?: boolean;
-};
-
-const ResumeCard = ({ resume, onDelete, deleting }: ResumeCardProps) => {
-  const { id, companyName, jobTitle, feedback, imagePath, resumePath } = resume;
+}) => {
   const { fs } = usePuterStore();
-  const [resumeUrl, setResumeUrl] = useState("");
+  const [resumeUrl, setResumeUrl] = useState('');
 
   useEffect(() => {
-    let url: string | null = null;
     const loadResume = async () => {
+      console.log("Loading image:", imagePath);
       const blob = await fs.read(imagePath);
       if (!blob) return;
-      url = URL.createObjectURL(blob);
+      let url = URL.createObjectURL(blob);
       setResumeUrl(url);
     };
     loadResume();
-
-    return () => {
-      if (url) {
-        URL.revokeObjectURL(url);
-      }
-    };
-  }, [fs, imagePath]);
+  }, [imagePath]);
 
   return (
-    <div className="relative resume-card animate-in fade-in duration-1000 my-10">
-      <DeleteButton
-        onClick={() => onDelete(id, resumePath, imagePath)}
-        disabled={deleting}
-      />
-
-      <Link to={`/resume/${id}`} className="block">
-        <div className="resume-card-header">
-          <div className="flex flex-col gap-2">
-            {companyName && (
-              <h2 className="text-black font-bold break-words">{companyName}</h2>
-            )}
-            {jobTitle && (
-              <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
-            )}
-            {!companyName && !jobTitle && (
-              <h2 className="text-black font-bold">Resume</h2>
-            )}
-          </div>
-          <div className="flex-shrink-0">
-            <ScoreCircle score={feedback.overallScore} />
+    <Link
+      to="{'/resume/${id}'}"
+      className="resume-card animate-in fade-in duration-1000 my-10"
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          {companyName && (
+            <h2 className="text-black font-bold break-words">{companyName}</h2>
+          )}
+          {jobTitle && (
+            <h3 className="text=lg break-words text-gray-500">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="text-black font-bold">Resume</h2>
+          )}
+        </div>
+        <div className="flex-shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      {resumeUrl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
           </div>
         </div>
-        {resumeUrl && (
-          <div className="gradient-border animate-in fade-in duration-1000">
-            <div className="w-full h-full">
-              <img
-                src={resumeUrl}
-                alt="resume"
-                className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-              />
-            </div>
-          </div>
-        )}
-      </Link>
-    </div>
+      )}
+    </Link>
   );
 };
 
